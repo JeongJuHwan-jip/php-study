2c54c387d7421866c594ef6eda3782b6e8b963e2
정주환 <55078363+JeongJuHwan-jip@users.noreply.github.com>
2023-10-27 23:22:49 +0900

    php

---
+++ array.php
@@ -0,0 +1,5 @@
+<?php
+    $coworkers = array('a', 'b', 'c');
+    array_push($coworkers, 'd');
+    var_dump(count($coworkers));
+?>
---
+++ function2.php
@@ -0,0 +1,15 @@
+<?php
+    function basic() {
+        echo "asdasdasd";
+    }
+    basic();
+    basic();
+    basic();
+?>
+
+<?php
+    function sum($a, $b) {
+        return $a+$b;
+    }
+    echo sum(1,2); 
+?>
